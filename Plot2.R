02b3073cfcbe4481d2e15af8cec49f5784006b5b
Kristineloh <kristineloh@hotmail.com>
2015-12-12 15:45:14 +0800

    Exploratory Data Analysis Project 1

---
+++ Plot2.R
@@ -0,0 +1,24 @@
+##Download file
+if(!file.exists("./data2")){dir.create("./data2")}
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileUrl,destfile="./data2/Dataset.zip",method="curl")
+
+##Unzip file
+unzip(zipfile="./data2/Dataset.zip",exdir="./data2")
+
+##Read file and preparing data
+power <- read.table("./data2/household_power_consumption.txt", header=T, sep=";")
+power$Date <- as.Date(power$Date, format="%d/%m/%Y")
+df <- power[(power$Date=="2007-02-01") | (power$Date=="2007-02-02"),]
+df$Global_active_power <- as.numeric(as.character(df$Global_active_power))
+df$Global_reactive_power <- as.numeric(as.character(df$Global_reactive_power))
+df$Voltage <- as.numeric(as.character(df$Voltage))
+df <- transform(df, timestamp=as.POSIXct(paste(Date, Time)), "%d/%m/%Y %H:%M:%S")
+df$Sub_metering_1 <- as.numeric(as.character(df$Sub_metering_1))
+df$Sub_metering_2 <- as.numeric(as.character(df$Sub_metering_2))
+df$Sub_metering_3 <- as.numeric(as.character(df$Sub_metering_3))
+
+##Plot 2
+png("Plot2.png", width=480, height=480)
+plot(df$timestamp, df$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
